38543f6b97ed88d42f9d7ad5ff9e36313434e5af
toori67 <toori67@gmail.com>
2016-10-24 22:27:40 +0900

    json transform test

--- src/test/java/org/iotlabs/dashboard/blueprints/TestBluePrint.java
+++ src/test/java/org/iotlabs/dashboard/blueprints/TestBluePrint.java
@@ -1,5 +1,8 @@
 package org.iotlabs.dashboard.blueprints;
 
+import com.google.gson.annotations.SerializedName;
+import org.iotlabs.dashboard.transformer.JsonTransformer;
+
 import static spark.Spark.get;
 
 public class TestBluePrint extends AbstractBluePrint  {
@@ -11,5 +14,18 @@ public class TestBluePrint extends AbstractBluePrint  {
     @Override
     public void register() {
         get(getEndPoint("/"), (req, res)  -> "test");
+        get(getEndPoint("json"), (req, res)-> {
+            TestModel testModel = new TestModel();
+            testModel.id = 1;
+            testModel.user = "test";
+            return testModel;
+        }, new JsonTransformer());
+    }
+
+    private static class TestModel {
+        @SerializedName("id")
+        private int id;
+        @SerializedName("user")
+        private String user;
     }
 }
