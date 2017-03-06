50191dab34e8eb39c5ea138c159924da8e493ea8
JongKwang Kim <kim@jongkwang.com>
2017-03-06 12:43:09 +0900

    Initialize bar charts and icon charts.

---
+++ Platforms/react-native/IoTLabs/Graph/BarGraph.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+
+} from 'react-native';
+
+import * as Animatable from 'react-native-animatable';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+
+export default class RowGraph extends Component {
+
+    componentWillMount(){
+    }
+    componentDidMount(){
+    }
+    barDate(){
+
+    }
+    render() {
+
+    }
+}
+
+module.exports = RowGraph;
---
+++ Platforms/react-native/IoTLabs/Graph/IconChar.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+
+} from 'react-native';
+
+import * as Animatable from 'react-native-animatable';
+import Entypo from 'react-native-vector-icons/Entypo';
+
+
+export default class RowGraph extends Component {
+
+    componentWillMount(){
+        console.log(this.state);
+    }
+    componentDidMount(){
+    }
+    render() {
+
+    }
+}
+
+module.exports = RowGraph;
