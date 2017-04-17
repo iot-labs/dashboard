b35447be0155c42a4f715f88c2c5ef68d0c91a46
유동영 <ydy0289@gmail.com>
2017-04-17 16:02:57 +0900

    Add Graph

--- Platforms/react-native/IoTLabs/Page/Dashboard.js
+++ Platforms/react-native/IoTLabs/Page/Dashboard.js
@@ -19,31 +19,48 @@ import {
 } from 'react-native';
 
 import * as Animatable from 'react-native-animatable';
+import LeftMenu from '../Commons/LeftMenu';
 import TopBar from '../Commons/TopBar';
 import RowGraph from '../Graph/RowGraph';
 import IconChar from '../Graph/IconChar';
-import BarGraph from '../Graph/BarGraph';
+import BarGraph1 from '../Graph/BarGraph1';
+import BarGraph2 from '../Graph/BarGraph2';
 
 
 export default class Dashboard extends Component {
+    state = {
+        leftManu : false,
+    }
     componentWillMount(){
     }
     componentDidMount(){
+    }
+    leftMenu(){
+        if(this.state.leftManu){
+            return (<LeftMenu/>);
+        }
     }
     render() {
         return (
             <View style={styles.container}>
                 <TopBar/>
-                <ScrollView>
-                <BarGraph size={GLOBAL.width*0.9}
-                          maxdeta={120}
-                          deta={GLOBAL.deta.airDust1}
-                          title="타이틀123"
-                />
-                <RowGraph size={GLOBAL.width*0.9} title="타이틀556" value1={72} maxValue={100}/>
-                <IconChar size={GLOBAL.width*0.9} title="타이틀" value={72} color="#2FA6D9"/>
-                <IconChar size={GLOBAL.width*0.9} title="타이틀" value={72} color="#04D6B2"/>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <IconChar size={GLOBAL.width*0.9} title="타이틀" value={72} color="#2FA6D9"/>
+                    <RowGraph size={GLOBAL.width*0.9} title="타이틀556" value1={72} maxValue={100} color="#2FA6D9"/>
+                    <IconChar size={GLOBAL.width*0.9} title="타이틀" value={72} color="#04D6B2"/>
+                    <BarGraph1 size={GLOBAL.width*0.9}
+                               maxdeta={120}
+                               deta={GLOBAL.deta.airDust1}
+                               title="타이틀123"
+                    />
+                    <IconChar size={GLOBAL.width*0.9} title="타이틀" value={72} color="#2FA6D9"/>
+                    <BarGraph2 size={GLOBAL.width*0.9}
+                               maxdeta={120}
+                               deta={GLOBAL.deta.airDust1}
+                               title="타이틀123"
+                    />
                 </ScrollView>
+                {this.leftMenu()}
             </View>
         );
     }
