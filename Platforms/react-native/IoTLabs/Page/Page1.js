0deb657a18d3e05ef258598e200f86589e8e5700
유동영 <ydy0289@gmail.com>
2017-04-18 13:33:32 +0900

    Add dashboard to living room

---
+++ Platforms/react-native/IoTLabs/Page/Page1.js
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Dimensions,
+    AsyncStorage,
+    Platform,
+    AppState,
+    ScrollView
+} from 'react-native';
+
+import * as Animatable from 'react-native-animatable';
+import LeftMenu from '../Commons/LeftMenu';
+import TopBar from '../Commons/TopBar';
+import RowGraph from '../Graph/RowGraph';
+import IconChar from '../Graph/IconChar';
+import BarGraph1 from '../Graph/BarGraph1';
+import BarGraph2 from '../Graph/BarGraph2';
+
+
+export default class Dashboard extends Component {
+    state = {
+        type: this.props.type,
+    }
+    componentWillMount(){
+    }
+    componentDidMount(){
+    }
+    leftMenu(){
+        if(this.state.leftManu){
+            return (<LeftMenu/>);
+        }
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <TopBar/>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <IconChar size={GLOBAL.width*0.9} title="거실 온도" value={27.5} color="#2FA6D9"/>
+                    <RowGraph size={GLOBAL.width*0.9} title="거실의 미세먼지 지수" value1={24} maxValue={100} color="#2FA6D9"/>
+                </ScrollView>
+                {this.leftMenu()}
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        backgroundColor: '#F2F4F8',
+    },
+});
+
+module.exports = Dashboard;
--- Platforms/react-native/IoTLabs/Page/index.js
+++ Platforms/react-native/IoTLabs/Page/index.js
@@ -17,10 +17,16 @@ import {
 
 import Intros from '../Page/Intro';
 import Dashboards from '../Page/Dashboard';
+import Page1s from '../Page/Page1';
+import Page2s from '../Page/Page2';
+import Page3s from '../Page/Page3';
+import Page4s from '../Page/Page4';
+import Page5s from '../Page/Page5';
+import Page6s from '../Page/Page6';
 
 var Index = React.createClass({
     componentWillMount(){
-        setTimeout(() => this.changeComponent("Dashboard"), 500);
+        setTimeout(() => this.changeComponent("Dashboard"), 100);
     },
     getInitialState: function() { return {componentSelected: 'Intro'}},
     changeComponent: function(component) {this.setState({componentSelected: component})},
@@ -30,6 +36,24 @@ var Index = React.createClass({
         }
         else if(component == 'Dashboard') {
             return <Dashboard changeComponent={this.changeComponent} />
+        }
+        else if(component == 'Page1') {
+            return <Page1 changeComponent={this.changeComponent} />
+        }
+        else if(component == 'Page2') {
+            return <Page2 changeComponent={this.changeComponent} />
+        }
+        else if(component == 'Page3') {
+            return <Page3 changeComponent={this.changeComponent} />
+        }
+        else if(component == 'Page4') {
+            return <Page4 changeComponent={this.changeComponent} />
+        }
+        else if(component == 'Page5') {
+            return <Page5 changeComponent={this.changeComponent} />
+        }
+        else if(component == 'Page6') {
+            return <Page6 changeComponent={this.changeComponent} />
         }
     },
     /*전체 감싸는 페이지*/
@@ -44,6 +68,12 @@ var Index = React.createClass({
 
 var Intro = React.createClass({render: function() {return (<Intros/>)}})
 var Dashboard = React.createClass({render: function() {return (<Dashboards/>)}})
+var Page1 = React.createClass({render: function() {return (<Page1s/>)}})
+var Page2 = React.createClass({render: function() {return (<Page2s/>)}})
+var Page3 = React.createClass({render: function() {return (<Page3s/>)}})
+var Page4 = React.createClass({render: function() {return (<Page4s/>)}})
+var Page5 = React.createClass({render: function() {return (<Page5s/>)}})
+var Page6 = React.createClass({render: function() {return (<Page6s/>)}})
 
 
 const styles = StyleSheet.create({
